0837d72d6d8cfe4ed0decb2540ccff75759a1a55
reza <nasirir80@gmail.com>
2025-09-29 08:24:56 +0330

    adminProfile

--- src/components/AdminProfile.jsx
+++ src/components/AdminProfile.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import Button from "./Button";
 
 const AdminProfile = () => {
   return (
-    <div>AdminProfile</div>
-  )
-}
+    <div className="flex flex-col w-full h-full items-start justify-center gap-6 lg:mr-4">
+      <div className="space-y-4 w-full">
+        <label className="block" htmlFor="name">
+          نام شما
+        </label>
+        <input
+          id="name"
+          type="text"
+          placeholder="reza"
+          className="border border-strok p-4 rounded-md block w-full max-w-[380px] outline-none"
+        />
+      </div>
+      <div className="space-y-4 w-full">
+        <label className="block" htmlFor="phoneNumber">
+          شماره تماس
+        </label>
+        <input
+          type="tel"
+          id="phoneNumber"
+          placeholder="0999999999"
+          className="border border-strok p-4 rounded-md block w-full max-w-[380px] outline-none"
+        />
+      </div>
+      <div className="space-y-4 w-full">
+        <label className="block" htmlFor="email">
+          ایمیل
+        </label>
+        <input
+          type="email"
+          id="email"
+          placeholder="OoU2t@example.com"
+          className="border border-strok p-4 rounded-md block w-full max-w-[380px] outline-none"
+        />
+      </div>
+      <div className="mt-10">
+        <Button style={"inline-block"} strok text={"تغییر رمز عبور"} />
+        <Button style={"inline-block mr-4"} fill text={"ذخیره تغییرات"} />
+      </div>
+    </div>
+  );
+};
 
-export default AdminProfile
+export default AdminProfile;
--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React from "react";
+import Button from "./Button";
+import { Instagram, MessageCircle, Send, X } from "lucide-react";
 
 const SocialMedia = () => {
   return (
-    <div>SocialMedia</div>
-  )
-}
+    <div className="flex flex-col w-full h-full items-start justify-center gap-6 lg:mr-4">
+      <div className="space-y-4 w-full">
+        <label className="block" htmlFor="instagram">
+          اینستاگرام
+        </label>
+        <div className="border border-strok rounded-sm flex items-center justify-end gap-2 w-full p-4 max-w-[380px]">
+          <input
+            type="text"
+            id="instagram"
+            placeholder="https://www.instagram.com/"
+            className="w-full h-full text-left outline-none"
+          />
+          <Instagram size={16} />
+        </div>
+      </div>
+      <div className="space-y-4 w-full">
+        <label className="block" htmlFor="telegram">
+          تلگرام
+        </label>
+        <div className="border border-strok rounded-sm flex items-center justify-end gap-2 w-full p-4 max-w-[380px]">
+          <input
+            type="text"
+            id="telegram"
+            placeholder="https://t.me/"
+            className="w-full h-full text-left outline-none"
+          />
+          <Send size={16} />
+        </div>
+      </div>
+      <div className="space-y-4 w-full">
+        <label className="block" htmlFor="whatsapp">
+          واتساپ
+        </label>
+        <div className="border border-strok rounded-sm flex items-center justify-end gap-2 w-full p-4 max-w-[380px]">
+          <input
+            type="text"
+            id="whatsapp"
+            placeholder="https://wa.me/"
+            className="w-full h-full text-left outline-none"
+          />
+          <MessageCircle size={16} />
+        </div>
+      </div>
+      <div className="space-y-4 w-full">
+        <label className="block" htmlFor="x">
+          ایکس
+        </label>
+        <div className="border border-strok rounded-sm flex items-center justify-end gap-2 w-full p-4 max-w-[380px]">
+          <input
+            type="text"
+            id="x"
+            placeholder="https://www.x.com/"
+            className="w-full h-full text-left outline-none"
+          />
+          <X size={16} />
+        </div>
+      </div>
 
-export default SocialMedia
+      <div className="mt-10">
+        <Button style={"inline-block mr-4"} fill text={"ذخیره تغییرات"} />
+      </div>
+    </div>
+  );
+};
+
+export default SocialMedia;
